a604cbbfccfe05d76883da40a086414054c9d3e1
Richard Webster <rdwebster@gmail.com>
2010-02-02 16:38:19 -0800

    OpenQuark version 1.7.2_4.
    Includes fixes to always build Java 1.5 class files.

--- src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
+++ src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
@@ -155,7 +155,7 @@ public final class Version {
     private static final int updateVersion = 2;
     
     /** build should be incremented by 1 for each build. */
-    private static final int build = 3;
+    private static final int build = 4;
     
     private Version() {}
     
