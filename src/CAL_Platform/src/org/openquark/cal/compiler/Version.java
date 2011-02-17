33b31ea75ba1a8039b5f80519e0194db357fa31c
Richard Webster <richardw@indicee.com>
2011-02-17 09:09:38 -0800

    OpenQuark build 1.7.2_18

--- src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
+++ src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
@@ -155,7 +155,7 @@ public final class Version {
     private static final int updateVersion = 2;
     
     /** build should be incremented by 1 for each build. */
-    private static final int build = 17;
+    private static final int build = 18;
     
     private Version() {}
     
