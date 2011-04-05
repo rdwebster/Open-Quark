493626d4632bc20d391883a4ce34831912278abe
Richard Webster <richardw@indicee.com>
2011-04-05 12:23:20 -0700

    Added TimeZone arguments to some BusinessCalendar functions.
    Previously, the 'currrent' time zone was used for these.
    
    OpenQuark build 1.7.2_22

--- src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
+++ src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
@@ -155,7 +155,7 @@ public final class Version {
     private static final int updateVersion = 2;
     
     /** build should be incremented by 1 for each build. */
-    private static final int build = 21;
+    private static final int build = 22;
     
     private Version() {}
     
