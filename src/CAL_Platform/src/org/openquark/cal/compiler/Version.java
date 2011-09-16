fd1d21f76c44886d2ec0ad65dfb7ad2453533f2b
Richard Webster <richardw@indicee.com>
2011-09-16 15:53:28 -0700

    OpenQuark build 1.7.2_29
    Changed Sql.makeGroupingQuery such that groupings tend to be on larger expressions instead of just on fields.

--- src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
+++ src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
@@ -155,7 +155,7 @@ public final class Version {
     private static final int updateVersion = 2;
     
     /** build should be incremented by 1 for each build. */
-    private static final int build = 28;
+    private static final int build = 29;
     
     private Version() {}
     
