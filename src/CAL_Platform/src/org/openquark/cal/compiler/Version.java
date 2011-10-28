24d08a57a56bb2b90193baefe6be4a0a10233579
Richard Webster <richardw@indicee.com>
2011-10-27 17:11:36 -0700

    OpenQuark build 1.7.2_32
    Fixed problem determining the time range for a calendar period smaller than a day.

--- src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
+++ src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
@@ -155,7 +155,7 @@ public final class Version {
     private static final int updateVersion = 2;
     
     /** build should be incremented by 1 for each build. */
-    private static final int build = 31;
+    private static final int build = 32;
     
     private Version() {}
     
