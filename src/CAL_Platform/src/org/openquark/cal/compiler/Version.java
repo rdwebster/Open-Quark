fd825f74d20d612e5c4eed54107b9a6d06208d86
Richard Webster <richardw@indicee.com>
2012-03-28 09:30:34 -0700

    OpenQuark build 1.7.2_43
    New Format and Debug utility functions.

--- src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
+++ src/CAL_Platform/src/org/openquark/cal/compiler/Version.java
@@ -155,7 +155,7 @@ public final class Version {
     private static final int updateVersion = 2;
     
     /** build should be incremented by 1 for each build. */
-    private static final int build = 42;
+    private static final int build = 43;
     
     private Version() {}
     
